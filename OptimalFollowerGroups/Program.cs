d04718e9e2b190617078c86f1c64c18d49e1e521
Andrew Green <eatmorehippo@gmail.com>
2014-12-13 14:45:20 -0800

    Removed some features found to be useless and spruced up the output.

--- OptimalFollowerGroups/Program.cs
+++ OptimalFollowerGroups/Program.cs
@@ -18,10 +18,17 @@ namespace OptimalFollowerGroups
 	public class Follower
 	{
 		public string name;
-		public int[] abilities = new int[2];
+		public int[] abilities = new int[3];
 
-		public string[] traits = new string[3];
 		public string race;
+
+		public string[] race_friends = new string[0];
+		public string[] trait_counter = new string[0];
+
+		public bool combat_experience = false;
+		public bool high_stamina = false;
+		public bool burst_of_power = false;
+		public bool epic_mount = false;
 
 		public double missions_completed = 0;
 		public double missions_completed_2 = 0;
@@ -85,97 +92,38 @@ namespace OptimalFollowerGroups
 				double prescence = 3;
 				foreach (Follower f in follower_combo)
 				{
-					if (mission.abilities[f.abilities[0]] != 0)
+					foreach(int ability in f.abilities)
 					{
-						f.missions_completed++;
-						mission.abilities[f.abilities[0]]--;
-						prescence += 3;
-					}
-					if (f.abilities[1] > -1 && mission.abilities[f.abilities[1]] != 0)
-					{
-						f.missions_completed++;
-						mission.abilities[f.abilities[1]]--;
-						prescence += 3;
+						if(ability >= 0 && mission.abilities[ability] != 0)
+						{
+							f.missions_completed++;
+							mission.abilities[ability]--;
+							prescence += 3;
+						}
 					}
 
-					List<string> slayer = new List<string>();
-					List<string> friend = new List<string>();
-					foreach (string trait in f.traits)
-						switch (trait)
-						{
-							case "Beastslayer":
-								slayer.Add("Beast");
-								break;
-							case "Brew Aficionado":
-								friend.Add("Panda");
-								break;
-							case "Cave Dweller":
-								slayer.Add("Underground");
-								break;
-							case "Child of Draenor":
-								friend.Add("Orc");
-								break;
-							case "Cold - Blooded":
-								slayer.Add("Snow");
-								break;
-							case "Combat Experience":
-								prescence += 1;
-								break;
-							case "Death Fascination":
-								friend.Add("Undead");
-								break;
-							case "Demonslayer":
-								slayer.Add("Demon");
-								break;
-							case "Elvenkind":
-								friend.Add("Blood elf");
-								break;
-							case "Furyslayer":
-								slayer.Add("Fury");
-								break;
-							case "Gronnslayer":
-								slayer.Add("Breaker");
-								break;
-							case "Guerilla Fighter":
-								slayer.Add("Jungle");
-								break;
-							case "Mountaineer":
-								slayer.Add("Mountain");
-								break;
-							case "Naturalist":
-								slayer.Add("Forest");
-								break;
-							case "Ogreslayer":
-								slayer.Add("Ogre");
-								break;
-							case "Orcslayer":
-								slayer.Add("Orc");
-								break;
-							case "Plainsrunner":
-								slayer.Add("Plains");
-								break;
-							case "Primalslayer":
-								slayer.Add("Primal");
-								break;
-							case "Talonslayer":
-								slayer.Add("Arrakoa");
-								break;
-							case "Voidslayer":
-								slayer.Add("Void");
-								break;
-							case "Voodoo Zealot":
-								friend.Add("Troll");
-								break;
-							case "Wastelander":
-								slayer.Add("Desert");
-								break;
-						}
-					foreach (string s in slayer)
+					//apply traits to missions
+					foreach (string s in f.trait_counter)
 						if (mission.trait == s)
 							prescence += 1;
-					foreach (string s in friend)
+					foreach (string s in f.race_friends)
 						foreach (Follower friend_maybe in follower_combo)
-							if (friend_maybe != f && friend_maybe.race == s) prescence += 1.5;
+							if (friend_maybe != f && friend_maybe.race == s)
+							{
+								prescence += 1.5;
+								break;
+							}
+					if (f.combat_experience)
+						prescence++;
+					if (f.burst_of_power || f.high_stamina)
+					{
+						bool mounted = false;
+						foreach (Follower friend_maybe in follower_combo)
+							if (friend_maybe.epic_mount == true)
+								mounted = true;
+						if (!mounted && f.high_stamina) prescence++;
+						if (mounted && f.burst_of_power) prescence++;
+					}
 				}
 
 				//Set last value to larger of the two, your best and this run
@@ -199,19 +147,8 @@ namespace OptimalFollowerGroups
 		{
 			using (StreamWriter writer = new StreamWriter("WriteText.txt"))
 			{
-				List<Follower> followers = new List<Follower>();
-
-				writer.Write(DateTime.Now);
-				//Create all combinations of abilities (no repeats)
-				writer.WriteLine("Getting all ability combinations...");
 				const int ability_count = 9;
-				for (int i = 0; i < ability_count; i++)
-					for (int j = i + 1; j < ability_count; j++)
-						followers.Add(new Follower() { abilities = new int[] { i, j } });
-
-				//Create all combination of n followers
-				writer.WriteLine("Getting all follower combinations...");
-				List<List<Follower>> combination_followers = Subset(followers, 4);
+				writer.Write(DateTime.Now);
 
 				//Open excel document
 				writer.WriteLine("Reading file...");
@@ -235,52 +172,6 @@ namespace OptimalFollowerGroups
 					missions.Last().trait = excel_reader.GetString(ability_count);
 				}
 
-				//Setup groups
-				writer.WriteLine("Setting up groups...");
-				var groups = new List<Group>();
-				foreach (List<Follower> group in combination_followers)
-					groups.Add(new Group() { followers = group });
-
-				foreach (Group g in groups) g.SetupCombos();
-
-				//Run through each mission and record success rate
-				writer.WriteLine("Testing groups...");
-				foreach (Group group in groups)
-					foreach (Mission mission in missions)
-						group.RunMission(mission);
-
-				//Sort by the success rates of the groups
-				groups.Sort((a, b) => b.total_ability_used.CompareTo(a.total_ability_used));
-
-				//Print out results for top groups
-				const int counter = 100;
-				for (int i = 0; i < Math.Min(groups.Count, counter); i++)
-				{
-					for (int j = 0; j < groups[i].followers.Count; j++)
-						writer.WriteLine(String.Format("Follower #{0}: {1}, {2}", j + 1, ability_names[groups[i].followers[j].abilities[0]], ability_names[groups[i].followers[j].abilities[1]]));
-					writer.WriteLine(String.Format("{0}/{1}\n", groups[i].total_ability_used, missions.Count * 6));
-				}
-
-				//Get the final results and print those out
-				var follower_stats_overall = new Dictionary<int, Follower>();
-				for (int i = 0; i < Math.Min(groups.Count, counter); i++)
-				{
-					foreach (Follower follower in groups[i].followers)
-					{
-						int calc_key = follower.abilities[0] * 100000000 + follower.abilities[1];
-						if (follower_stats_overall.ContainsKey(calc_key))
-							follower_stats_overall[calc_key].missions_completed += follower.missions_completed;
-						else
-							follower_stats_overall[calc_key] = new Follower() { abilities = follower.abilities, missions_completed = follower.missions_completed };
-					}
-				}
-
-				List<Follower> followers_sorted = follower_stats_overall.Values.ToList();
-				followers_sorted.Sort((a, b) => a.missions_completed.CompareTo(b.missions_completed));
-				followers_sorted.Reverse();
-				foreach (Follower follower_cumulative in followers_sorted)
-					writer.WriteLine(String.Format("{0}: {1}, {2}", follower_cumulative.missions_completed, ability_names[follower_cumulative.abilities[0]], ability_names[follower_cumulative.abilities[1]]));
-
 				//Open excel file for followers
 				writer.WriteLine("Reading followers file...");
 				stream = File.Open("followers.xlsx", FileMode.Open, FileAccess.Read);
@@ -295,21 +186,33 @@ namespace OptimalFollowerGroups
 				var ability_names_list = new List<String>(ability_names);
 				while (excel_reader.Read())
 				{
+					int c = 0;
 					var f = new Follower();
-					f.name = excel_reader.GetString(0);
-					f.race = excel_reader.GetString(1);
+					f.name = excel_reader.GetString(c++);
+					f.race = excel_reader.GetString(c++);
 
-					f.abilities[0] = ability_names_list.IndexOf(excel_reader.GetString(2));
-					f.abilities[1] = ability_names_list.IndexOf(excel_reader.GetString(3));
+					f.abilities[0] = ability_names_list.IndexOf(excel_reader.GetString(c++));
+					f.abilities[1] = ability_names_list.IndexOf(excel_reader.GetString(c++));
+					f.abilities[2] = ability_names_list.IndexOf(excel_reader.GetString(c++));
 
-					f.traits[0] = excel_reader.GetString(4);
-					f.traits[1] = excel_reader.GetString(5);
-					f.traits[2] = excel_reader.GetString(6);
+					string race = excel_reader.GetString(c++);
+					if(!string.IsNullOrWhiteSpace(race))
+						f.race_friends = race.Split(new string[] { ", " }, StringSplitOptions.None);
+
+					string traits = excel_reader.GetString(c++);
+					if (!string.IsNullOrWhiteSpace(traits))
+						f.trait_counter = traits.Split(new string[] { ", " }, StringSplitOptions.None);
+
+					f.combat_experience = !string.IsNullOrWhiteSpace(excel_reader.GetString(c++));
+					f.high_stamina = !string.IsNullOrWhiteSpace(excel_reader.GetString(c++));
+					f.burst_of_power = !string.IsNullOrWhiteSpace(excel_reader.GetString(c++));
+					f.epic_mount = !string.IsNullOrWhiteSpace(excel_reader.GetString(c++));
 
 					my_followers.followers.Add(f);
 				}
 
 				//Run followers on missions
+				writer.WriteLine("\nBest followers for each mission");
 				foreach (Mission mission in missions)
 				{
 					my_followers.RunMission(mission);
@@ -323,12 +226,20 @@ namespace OptimalFollowerGroups
 				}
 
 				//Print follower information out
+				writer.WriteLine("\nNumber of missions each follower went on");
 				my_followers.followers.Sort((a, b) => a.missions_completed_2.CompareTo(b.missions_completed_2));
 				my_followers.followers.Reverse();
 				foreach (Follower follower in my_followers.followers)
 					writer.WriteLine(String.Format("{0}: {1}", follower.name, follower.missions_completed_2));
 
 				//Find mr perfect amongst all the possible combinations
+				//Create all combinations of abilities (no repeats)
+				writer.WriteLine("\nLargest improvement combos");
+				List<Follower> followers = new List<Follower>();
+				for (int i = 0; i < ability_count; i++)
+					for (int j = i + 1; j < ability_count; j++)
+						followers.Add(new Follower() { abilities = new int[] { i, j } });
+
 				List<double> mission_prescence = new List<double>();
 				double sum_prescence = missions.Sum(item => item.prescence);
 				List<Tuple<Follower, double>> track_best_followers = new List<Tuple<Follower, double>>();
